d8718a4ccdc813a7384e596671902a5ec9725e4b
biancapadure <73990941+biancapadure@users.noreply.github.com>
2022-03-03 17:39:31 +0000

    Add files via upload

---
+++ basket.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basket</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+     <!-- <?php
+    include "topMenu.html";
+    include "sideMenu.html";
+    ?> -->
+
+    <h1>Basket</h1>
+
+    <table>
+        <tr>
+            <th>Item No</th>
+            <th>Product and code</th>
+            <th>Price (Per item)</th>
+            <th>No. of items</th>
+            <th>Color</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Jacket (A6)</td>
+            <td>£180.99</td>
+            <td>1</td>
+            <td>Green</td>
+            <td>Add</td>
+            <td>Remove</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Chair (B2)</td>
+            <td>£22.44</td>
+            <td>3</td>
+            <td>Black</td>
+            <td>Add</td>
+            <td>Remove</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Table (C4)</td>
+            <td>£40.55</td>
+            <td>1</td>
+            <td>White</td>
+            <td>Add</td>
+            <td>Remove</td>
+        </tr>
+    </table>
+
+    <br>
+    <button>Click to pay</button>
+
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- <?php
+    include "topMenu.html";
+    include "sideMenu.html";
+    ?> -->
+
+    <h1>Spring's Best Sellers</h1>
+    <br>
+
+    <div id="bestSellerItemsContainer">
+    <img class="bestSellerItem" src="img/placeholder.png">
+    <img class="bestSellerItem" src="img/placeholder.png">
+    <img class="bestSellerItem" src="img/placeholder.png">
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
