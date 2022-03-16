7676dc269427abc117963a895bd8737b6fc58a14
vaidarubyte2624 <80453951+vaidarubyte2624@users.noreply.github.com>
2022-03-16 16:10:17 +0000

    Update DbConnect.php

--- DbConnect.php
+++ DbConnect.php
@@ -3,10 +3,10 @@
 // Sets up the connection parameters to a mysql database
 
 
-DEFINE ('DB_USER', 'IN18019491');                           // The username
-DEFINE ('DB_PASSWORD','18019491');                        // The password
+DEFINE ('DB_USER', '');                           // The username
+DEFINE ('DB_PASSWORD','');                        // The password
 DEFINE ('DB_HOST', 'localhost');                        // The mysql server host address 
-DEFINE ('DB_NAME', 'IN18019491');                           // The database name
+DEFINE ('DB_NAME', '');                           // The database name
   @$DB = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
   if (mysqli_connect_errno())
     {
